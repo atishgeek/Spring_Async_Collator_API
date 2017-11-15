a8face29f68265c4877750034887c62508cd27b4
atishgeek <atishgeek@gmail.com>
2017-11-15 14:27:50 +0530

    Unwanted code removed

--- src/main/java/com/imaginea/Launcher/SpringAsyncCollatorLauncher.java
+++ src/main/java/com/imaginea/Launcher/SpringAsyncCollatorLauncher.java
@@ -1,10 +1,7 @@
 package com.imaginea.Launcher;
 
-import static org.junit.Assert.*;
-
 import java.util.concurrent.Executor;
 
-import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
